dac2d805204c268d33c6feda0039d3b2a96c591d
Vitali Metelev <devita.work@yandex.ru>
2020-12-03 20:53:59 +0400

    Добавьте файлы проекта.

---
+++ WebApplication1/Models/Day.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Day: ICloneable
+    {
+        public string NameDay;
+        public Lesson[] Lessons;
+
+        public object Clone()
+        {
+            return new Day
+            {
+                NameDay = this.NameDay,
+                Lessons = this.Lessons.ToArray()
+            };
+        }
+    }
+}
---
+++ WebApplication1/Models/Lesson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Lesson : IComparable
+    {
+        public int Hour;
+        public int Minutes;
+        public string NameLesson;
+        public int CompareTo(object obj)
+        {
+            Lesson p = obj as Lesson;
+            if (p != null)
+                return this.NameLesson.CompareTo(p.NameLesson);
+            else
+                throw new Exception("Невозможно сравнить два объекта");
+        }
+    }
+
+    
+
+}
